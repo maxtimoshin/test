400bbc8e36480533df96359909a545e9a0a6bc72
Maxim94 <maxtimoshin94@gmail.com>
2021-06-17 00:28:22 +0300

    burger menu --added

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
-const signIn = document.querySelector('.sign-in')
+const signIn = document.querySelector('.header__sign-in')
 const basketAmount = document.querySelector('.header__amount')
 const inputs = document.querySelectorAll('.input')
 const header = document.querySelector('.header')
+const iconMenu = document.querySelector('.header__menu-icon')
+const menuBody = document.querySelector('.header__menu-body')
 
+
+
+if (iconMenu) {
+    iconMenu.addEventListener('click', (e) => {
+        document.body.classList.toggle('lock')
+        iconMenu.classList.toggle('active')
+        menuBody.classList.toggle('active')
+    })
+}
 signIn.addEventListener('click', () => {
     signIn.innerText = 'Hi, Angels!'
     basketAmount.style.display = 'flex'
